8848e2d2d43ba29d9d0d1414020bec10c952b2ee
Allen Fung <allentfung@gmail.com>
2013-12-02 04:27:33 +0000

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,9 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -93,5 +96,9 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
